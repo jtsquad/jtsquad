ecf97344171b976ffce3715ed65fa5f8970a3a4c
Anotso <40184359+Anotso@users.noreply.github.com>
2020-07-20 21:59:10 -0300

    Envio da lib typed e efeito de escrita de texto

---
+++ assets/js/animate.js
@@ -0,0 +1,8 @@
+var typed4 = new Typed('#alternation', {
+    strings: ['jovens', 'praticos', 'inovadores', 'JT Squad'],
+    typeSpeed: 30,
+    backSpeed: 30,
+    backDelay: 1000,
+    startDelay: 2000,
+    loop: true
+});
