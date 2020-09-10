d3ae170a5648e3ba60dd2e096e54d984f74a8c01
Danielle Rodrigues <31356718+DanielleRodrigues@users.noreply.github.com>
2020-09-10 03:37:29 -0300

    Add files via upload
     + limpeza de código

---
+++ js/animate.js
@@ -0,0 +1,8 @@
+var typed = new Typed('#alternation', {
+    strings: ['Inovação', 'Criatividade', 'Qualidade', 'JT Squad'],
+    typeSpeed: 30,
+    backSpeed: 30,
+    backDelay: 1000,
+    startDelay: 2000,
+    loop: true
+});     
---
+++ js/port.js
@@ -0,0 +1,56 @@
+/*******************************************sessao portfolio*******************************************/
+
+/*******Filtro*******/
+
+
+var $grid = $('.img-grid').isotope({
+    itemSelector: '.img-container',
+
+  });
+  
+  
+  var filterFns = {
+    numberGreaterThan50: function() {
+    },
+  };
+  
+  // conexao do botao 
+  $('#filter-btn').on( 'click', 'button', function() {
+    var filterValue = $( this ).attr('data-filter');
+    filterValue = filterFns[ filterValue ] || filterValue;
+    $grid.isotope({ filter: filterValue });
+  });
+  
+  //  checa a classe do botao
+  $('.button-group').each( function( i, buttonGroup ) {
+    var $buttonGroup = $( buttonGroup );
+    $buttonGroup.on( 'click', 'button', function() {
+      $buttonGroup.find('.is-checked').removeClass('is-checked');
+      $( this ).addClass('is-checked');
+    });
+  });
+  
+  /*******Galeria de imgs*******/
+  
+  $('.popup-gallery').magnificPopup({
+  type: 'image',
+  gallery:{
+    enabled:true
+  }
+  });
+  
+  $(document).ready(function(){
+    $('.loop').owlCarousel({
+      center: true,
+      items:1,
+      loop:true,
+      margin:10,
+      responsive:{
+        600:{
+          items:1
+        }
+      }
+    });
+  });
+  
+ 
